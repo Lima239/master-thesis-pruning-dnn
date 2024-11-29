399e4b9d8c559eb8cc69906ca2140307ed2df450
Lima239 <j.lichmanova@gmail.com>
2024-11-29 13:40:27 +0100

    run shell script added

---
+++ clustering_algorithms/run.sh
@@ -0,0 +1,7 @@
+
+python3 correlation_algorithms/whole_correlation/correlation_mip.py ../inputs/real_weights/fc1_81x81.pt > correlation_algorithms/whole_correlation/runs/fc1_8000.txt
+
+python3 correlation_algorithms/block_correlation/block_correlation_mip.py ../inputs/real_weights/fc1_81x81.pt > correlation_algorithms/block_correlation/runs/fc1_8000.txt
+
+python3 manhattan_algorithms/manhattan_L1_distance_mip.py ../inputs/real_weights/fc1_81x81.pt > manhattan_algorithms/runs/fc1_8000.txt
+
