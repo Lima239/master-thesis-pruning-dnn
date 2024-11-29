e8973c602ec265003b1bee4e4ecb3a7b79d86e87
Lima239 <j.lichmanova@gmail.com>
2024-11-29 13:41:02 +0100

    clean up

--- inputs/real_weights/downloadModelWeights.py
+++ inputs/real_weights/downloadModelWeights.py
@@ -1,13 +1,15 @@
 import torch
 
 def generate_and_save_tensor(file_path):
-    data = torch.load("/pytorch_model.bin")
-    matrix = data["decoder.layers.1.self_attn.v_proj.weight"]
+    data = torch.load("../../substitution_into_model/fc1.pt")
+    #matrix = data["decoder.layers.1.self_attn.v_proj.weight"]
 
+    matrix = data[:81, :81]
     torch.save(matrix, file_path)
+    print(matrix.shape)
     print(f'Tensor saved to {file_path}')
 
 if __name__ == "__main__":
-    output_file = 'pytorch_weights.pt'
+    output_file = 'fc1_81x81.pt'
 
     generate_and_save_tensor(output_file)
